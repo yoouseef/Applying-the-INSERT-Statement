c88190dbb106bc58007299c61c57ff073f873b7f
yoouseef <115207558+yoouseef@users.noreply.github.com>
2023-10-23 22:21:03 +0500

    Update INSERT_Xasanov_Yusuf.sql

--- INSERT_Xasanov_Yusuf.sql
+++ INSERT_Xasanov_Yusuf.sql
@@ -1,22 +1,23 @@
-INSERT INTO film (title, description, release_year, 
+INSERT INTO film (film_id, title, description, release_year, 
 				  language_id, rental_duration, rental_rate, length, rating, special_features )
-VALUES('Avangers: Infinity War', 'The heroes of the earth rise against evil called Thanos', 2019, 1, 14, 4.99, 136, 'R',Array['Trailer', 'Movie Theatre'])
+VALUES(2000, 'Avangers: Infinity War', 'The heroes of the earth rise against evil called Thanos', 2019, 1, 14, 4.99, 136, 'R',Array['Trailer', 'Movie Theatre'])
 
 
-INSERT INTO actor (first_name, last_name)
+INSERT INTO actor (actor_id, first_name, last_name)
 VALUES
-	('Tom', 'Holland'),
-	('Robert Downey', 'Junior'),
-	('Scarlet', 'Johanson')
+	(1000, 'Tom', 'Holland'),
+	(1001, 'Robert Downey', 'Junior'),
+	(1002, 'Scarlet', 'Johanson')
 	
 INSERT INTO film_actor (actor_id, film_id)
 VALUES
-	(201, 1001),
-	(202, 1001),
-	(203, 1001)
+	(1000, 2000),
+	(1001, 2000),
+	(1002, 2000)
 
 
 
 INSERT INTO inventory (film_id, store_id)
 VALUES
-	(1001, 2)
+	(2000, 2)
+	
